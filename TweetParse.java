a793f254c0cb52becbbf2004d0af4f119da565c3
Paul Gautreaux <paul@embdevspecialists.com>
2020-04-27 03:31:03 -0700

    More rearranging to match Spencer's order.

--- TweetParse.java
+++ TweetParse.java
@@ -4,21 +4,21 @@ import java.io.IOException;
 import java.util.*;
 import java.util.regex.*;
 
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.*;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
 
-import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
-import org.apache.hadoop.mapred.*;
-import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.InputFormat;
 import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
-import org.apache.hadoop.mapreduce.InputFormat;
 import org.apache.hadoop.util.*;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
 
 public class TweetParse {
  public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
