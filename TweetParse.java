88051e91812e27e679b2a30029bd9f0d3c61158a
Paul Gautreaux <paul@embdevspecialists.com>
2020-04-27 03:17:00 -0700

    Reorder classes to be similar order to Spencer's.

--- TweetParse.java
+++ TweetParse.java
@@ -21,6 +21,71 @@ import org.apache.hadoop.mapreduce.InputFormat;
 import org.apache.hadoop.util.*;
 
 public class TweetParse {
+ public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+    private Pattern pTime = Pattern.compile("(\\d+):(\\d+):(\\d+)");
+
+    public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter)
+        throws IOException {
+      String line = value.toString();
+      StringTokenizer tokenizer = new StringTokenizer(line);
+
+      if (line.length() > 0) {
+        switch (line.charAt(0)) {
+          case 'T':
+            // We have a time entry, tokenize it.
+            if (tokenizer.hasMoreTokens()) {
+              tokenizer.nextToken(); // skip over 'T'
+              tokenizer.nextToken(); // skip over date
+
+              // match for time.
+              String recordTime = tokenizer.nextToken();
+              Matcher m = pTime.matcher(recordTime);
+              if (m.matches()) {
+                word.set("" + m.group(1));
+                output.collect(word, one);
+              }
+            }
+            break;
+
+          case 'W':
+            while (tokenizer.hasMoreTokens()) {
+              String token = tokenizer.nextToken().toLowerCase();
+              if (token.contains("sleep")) {
+                //
+                // Uncomment to exclude sleep as a substring of other strings.
+                //
+                // if (token.length() > 5)
+                // {
+                // Character c = token.charAt(5);
+                // if (c >= 'a' && c <= 'z')
+                // {
+                // break;
+                // }
+                // }
+                word.set("sleep");
+                output.collect(word, one);
+                break;
+              }
+            }
+            break;
+        }
+      }
+    }
+  }
+  
+   public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+    public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output,
+        Reporter reporter) throws IOException {
+      int sum = 0;
+      while (values.hasNext()) {
+        sum += values.next().get();
+      }
+      output.collect(key, new IntWritable(sum));
+    }
+  } 
+  
   public class TweetReader extends RecordReader<LongWritable, Text> {
     private long start; // holds the starting position of the split
     private long pos;
@@ -178,69 +243,9 @@ public class TweetParse {
 
   }
 
-  public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
-    private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
-    private Pattern pTime = Pattern.compile("(\\d+):(\\d+):(\\d+)");
+ 
 
-    public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter)
-        throws IOException {
-      String line = value.toString();
-      StringTokenizer tokenizer = new StringTokenizer(line);
 
-      if (line.length() > 0) {
-        switch (line.charAt(0)) {
-          case 'T':
-            // We have a time entry, tokenize it.
-            if (tokenizer.hasMoreTokens()) {
-              tokenizer.nextToken(); // skip over 'T'
-              tokenizer.nextToken(); // skip over date
-
-              // match for time.
-              String recordTime = tokenizer.nextToken();
-              Matcher m = pTime.matcher(recordTime);
-              if (m.matches()) {
-                word.set("" + m.group(1));
-                output.collect(word, one);
-              }
-            }
-            break;
-
-          case 'W':
-            while (tokenizer.hasMoreTokens()) {
-              String token = tokenizer.nextToken().toLowerCase();
-              if (token.contains("sleep")) {
-                //
-                // Uncomment to exclude sleep as a substring of other strings.
-                //
-                // if (token.length() > 5)
-                // {
-                // Character c = token.charAt(5);
-                // if (c >= 'a' && c <= 'z')
-                // {
-                // break;
-                // }
-                // }
-                word.set("sleep");
-                output.collect(word, one);
-              }
-            }
-            break;
-        }
-      }
-    }
-  }
-
-  public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
-    public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output,
-        Reporter reporter) throws IOException {
-      int sum = 0;
-      while (values.hasNext()) {
-        sum += values.next().get();
-      }
-      output.collect(key, new IntWritable(sum));
-    }
-  }
 
   public static void main(String[] args) throws Exception {
     JobConf conf = new JobConf(TweetParse.class);
